ee7f181a989feac9c9a99bd58fa1382ce6f7016a
faiz8411 <faizullah.noa2014@gmail.com>
2022-03-01 13:06:02 +0600

    card end

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,16 +2,21 @@ import React, { useEffect } from 'react';
 import MovieListing from '../MovieListing/MovieListing';
 import {APIKey} from "../../common/apis/MovieApiKey";
 import MovieApi from "../../common/apis/MovieApi"
+import { useDispatch } from 'react-redux';
+import { addMovies } from '../../features/movies/moviesSlice';
 
 const Home = () => {
+    const movieText = "Harry";
+     const dispatch = useDispatch();
     useEffect(() => {
-        const movieText="Harry"
+        
+       
         const fetchMovies =async () => {
             const response = await MovieApi.get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
                 .catch(err => {
                 console.log(err)
                 })
-            console.log(response)
+            dispatch(addMovies(response.data))
         }
         fetchMovies()
     },[])
